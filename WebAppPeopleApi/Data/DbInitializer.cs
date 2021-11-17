83b64508d6ea4f32d659673d1cc59c03da2e0315
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-11-17 16:15:38 +0100

    Db init and seeding added

---
+++ WebAppPeopleApi/Data/DbInitializer.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppPeopleApi.Models.Entity;
+
+namespace WebAppPeopleApi.Data
+{
+    internal class DbInitializer
+    {
+        internal static void Initialize(AppDbContext context)
+        {
+            /*
+               Will create the database if it does not already exist.
+               applies any pending migrations for the context to the database.
+            */
+            context.Database.Migrate();
+            //context.Database.EnsureCreated();//If not using EF migrations.
+
+            if (context.People.Any())//Check if there are any people in the database, if there are end the Initialize here. 
+            {
+                return;
+            }
+
+            //Seeding in some people if there where not anyone in the database.
+            Person person1 = new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Mehrdad",
+                LastName = "Javan",
+                Email = "Mehrdad.Javan@Lexicon.se",
+                Title = "Teacher",
+                Created = DateTime.Now,
+                Modified = DateTime.Now
+            };
+            Person person2 = new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Ulf",
+                LastName = "Bengtsson",
+                Email = "Ulf.Bengtsson@Lexicon.se",
+                Title = "Teacher",
+                Created = DateTime.Now,
+                Modified = DateTime.Now
+            };
+            Person person3 = new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Fredrik",
+                LastName = "Odin",
+                Email = "Fredrik.Odin@Adapta.se",
+                Title = "Consultant",
+                Created = DateTime.Now,
+                Modified = DateTime.Now
+            };
+
+            context.People.Add(person1);
+            context.People.Add(person2);
+            context.People.Add(person3);
+
+            context.SaveChanges();
+        }
+    }
+}
--- WebAppPeopleApi/Program.cs
+++ WebAppPeopleApi/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAppPeopleApi.Data;
 
 namespace WebAppPeopleApi
 {
@@ -13,7 +15,11 @@ namespace WebAppPeopleApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +28,23 @@ namespace WebAppPeopleApi
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while creating the DB.");
+                }
+            }
+        }
     }
 }
